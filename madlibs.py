bb0a88a57e368b9697dbf29c5f2bbd1f096cbcac
Maya <mayas2744@tuckahoeschools.org>
2024-10-23 12:26:17 +0000

    init commit

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+adjective1 = input("Type an adjective: ")
+place1 = input("Type a place: ")
+name1 = input("Type a boy's name: ")
+age1 = input("Type an age: ")
+animal1 = input("Type an animal: ")
+name2 = input("Type a girl's name: ")
+emotion1 = input("Type an emotion: ")
+noun2 = input("Type a noun: ")
+verb1 = input("Type a past-tense action verb: ")
+verb2 = input("Type a past-tense action verb: ")
+noise1 = input("Type a noise: ")
+emotion2 = input("Type an emotion: ")
+adjective2 = input("Type an adjective: ")
+acrobatic1 = input("Type an acrobatic move: ")
+
+print(f"""Since the beginning of time there was a {adjective1} {place1}.
+{name1} used to visit this place when he was {age1}. 
+At the {place1}, he found an abandoned {animal1} which he adopted with the help of his friend {name2}. 
+{name1} and {name2} enjoyed raising the {animal1} and during the summer signed up their {animal1} for a circus performance.
+The {animal1} was an incredible performer and made the audience {emotion1}. 
+During the performance, {name2} forgot her {noun2} to record the show and {verb1} home to retrieve it.
+As she {verb2} through the door and entered her house, a loud {noise1} from behind made her jump. {name2}'s younger brother had stolen her {noun2} and broke it. 
+{name2} was {emotion2} and couldn't bear to be in the same room as her {adjective2} brother.
+Fortunately, she made it back to the show on time to see the {animal1} do a {acrobatic1}. 
+After the show, {name1} and {name2} went back home with their {animal1} and a gold medal for the {animal1}'s wonderful performance.""")
